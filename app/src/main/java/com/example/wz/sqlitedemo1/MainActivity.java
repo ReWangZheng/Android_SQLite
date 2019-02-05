32656fa30a1bad41ce8f965487701e832f838baa
ReWangZheng <29370330@qq.com>
2019-02-06 01:10:12 +0800

    这次会了SQlite数据的insert和update

--- app/src/main/java/com/example/wz/sqlitedemo1/MainActivity.java
+++ app/src/main/java/com/example/wz/sqlitedemo1/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.wz.sqlitedemo1;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +21,35 @@ public class MainActivity extends AppCompatActivity {
                 helper.getWritableDatabase();
             }
         });
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseHelper databaseHelper=new DatabaseHelper(MainActivity.this,"dbStu",null,4);
+                SQLiteDatabase sqld=databaseHelper.getWritableDatabase();
+                ContentValues values=new ContentValues();
+                values.put("author","Regan");
+                values.put("price",98.6);
+                values.put("pages",764);
+                values.put("name","《C primer plus》");
+                sqld.insert("Book",null,values);
+            }
+        });
+
+        Button button3=(Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase sdb=new DatabaseHelper(MainActivity.this,"dbStu",null,4).getWritableDatabase();
+                ContentValues values=new ContentValues();
+                values.put("name","《JAVA》");
+                sdb.update("Book",values,"name=?",new String[]{"《C primer plus》"});
+
+            }
+        });
+
+
 
     }
 }
